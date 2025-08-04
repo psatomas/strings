f5cfb12156a130f904ea4b175888ad867a79d5d6
psatomas <psatomas@gmail.com>
2025-08-04 20:22:37 -0300

    chore: Clone string values and slice first character

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let pirate = "Bloodhook";
+
+    let sailer = String::from(pirate);
+
+    let bad_guy = pirate.to_string();
+
+    println!("{pirate} and {sailer} and {bad_guy}");
+    
+    let first_initial = &pirate[0..1];
+    println!("{first_initial}");
 }
